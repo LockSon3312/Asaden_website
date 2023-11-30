02b83a1b69ff81d849b85e6e3a51af7d6c9fc2bf
LockSon3312 <151898689+LockSon3312@users.noreply.github.com>
2023-11-30 12:23:52 -0400

    Add files via upload

---
+++ js/reproductor.js
@@ -0,0 +1,24 @@
+/* Playlist*/
+
+
+
+// Verificar si se esta reproduciendo el audio
+
+/* Funcion Play*/
+
+/* Funcion Pausa*/
+
+/* Funcion Leer Cacnion*/
+
+/*Cancion Actual*/
+
+/*Anterior Cancion*/
+
+/*Siguiente cancion*/
+
+
+//al; cargar las cancioner, se leera la primera cancion
+
+
+
+//https://www.youtube.com/watch?v=_ObsZ5Bpz4Q
